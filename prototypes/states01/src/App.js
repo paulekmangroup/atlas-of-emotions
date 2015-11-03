dd44cfe3a6b083235523f3bd7323d13f0cddd511
ericsoco <eric@transmote.com>
2015-11-03 13:28:59 -0800

    Draw graph correctly, with gradients

--- prototypes/states01/src/App.js
+++ prototypes/states01/src/App.js
@@ -33,7 +33,7 @@ export default {
 			.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
 		// 
-		// d3 utils
+		// d3/svg setup
 		// 
 		let xScale = d3.scale.linear()
 			.domain([0, 10])
@@ -47,13 +47,33 @@ export default {
 			.scale(xScale)
 			.orient('bottom')
 			.ticks(10)
-			.tickFormat(String)
+			.tickFormat(d => '')
 			.tickSize(10);
 
 		let areaGenerator = d3.svg.area()
 			.x(d => xScale(d.x))
 			.y0(innerHeight)
 			.y1(d => yScale(d.y));
+
+		svg.append('defs')
+		.append('linearGradient')
+			.attr('id', 'anger-gradient')
+			.attr('gradientUnits', 'userSpaceOnUse')
+
+			// these will be set manually on each path's gradient
+			.attr('x1', xScale(0))
+			.attr('x2', xScale(10))
+
+			.attr('y1', yScale(0))
+			.attr('y2', yScale(0))
+		.selectAll('stop')
+			.data([
+				{ offset: '0%', color: 'rgba(228, 135, 102, 0.2)' },
+				{ offset: '100%', color: 'rgba(204, 28, 43, 1.0)' }
+			])
+		.enter().append('stop')
+			.attr('offset', d => d.offset)
+			.attr('stop-color', d => d.color);
 
 		//
 		// Draw graph
@@ -66,16 +86,39 @@ export default {
 		// transform state range into points for area chart
 		let transformedRanges = this.transformRanges(_.values(emotionsData.emotions.anger.states), 'anger');
 
-		stateGraphContainer.selectAll('path.area')
-			.data(transformedRanges)
-		.enter().append('path')
+		let stateElements = stateGraphContainer.selectAll('path.area')
+			.data(transformedRanges).enter();
+
+		stateElements.append('linearGradient')
+			.attr('xlink:href', '#anger-gradient')
+			.attr('id', (d, i) => 'anger-gradient-' + i)
+			.attr('x1', d => xScale(d[0].x))
+			.attr('x2', d => xScale(d[2].x));
+
+		stateElements.append('path')
 			.attr('class', 'area')
 			.attr('d', areaGenerator)
-			.attr('transform', (d => 'translate(' + xScale(d[0].x) + ',0)'));
+			.attr('fill', (d, i) => 'url(#anger-gradient-' + i + ')');
 
 	},
 
 	transformRanges: function (states, emotion) {
+
+		// sort by state min value, then max value
+		states = states.sort((a, b) => {
+			if (a.range.min < b.range.min) {
+				return -1;
+			} else if (a.range.min > b.range.min) {
+				return 1;
+			} else {
+				if (a.range.max < b.range.max) {
+					return -1;
+				} else if (a.range.max > b.range.max) {
+					return 1;
+				}
+			}
+			return 0;
+		});
 
 		const transformers = {
 			'anger': this.transformAngerRanges,
@@ -93,20 +136,23 @@ export default {
 
 		// in -> { min: a, max: b }
 		
-		return states.map(state => {
+		let numStates = states.length;
+		return states.map((state, i) => {
 
 			let points = [],
-				min = state.range.min - 1,	// transform to 0-indexed, and allow
-											// states with same min and max to display
+				min = state.range.min - 1,				// transform to 0-indexed, and allow
+														// states with same min and max to display
 				max = state.range.max,
-				spread = max - min;
+				spread = max - min,
+				offsetBase = 0.5 * (i / numStates);		// offsets skew left of center toward left edge
+														// of graph, right of center toward right
 
 			points.push({
 				x: min,
 				y: 0
 			});
 			points.push({
-				x: min + spread * (0.25 + 0.5 * Math.random()),
+				x: min + spread * (offsetBase + 0.5 * Math.random()),
 				y: min + 0.5 * spread
 			});
 			points.push({
