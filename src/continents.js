b6b91f69abd427aee372096ed3fcbe38c1aefddf
Eric Socolofsky <eric@transmote.com>
2015-11-15 15:00:57 -0800

    Set up transition plan for continents.

--- src/continents.js
+++ src/continents.js
@@ -21,7 +21,7 @@ let labelContainer,
 		freq: 0.015
 	},
 	frameCount = 0,
-	highlightedContinent = null;
+	currentEmotion = null;
 
 
 
@@ -68,7 +68,88 @@ const continentsSection = {
 
 	setEmotion: function (emotion) {
 
-		console.log(">>>>> TODO: continents.setEmotion should zoom into an emotion if zoomed out, or just render already zoomed in.");
+		if (emotion === currentEmotion) { return; }
+
+		if (currentEmotion) {
+
+			// -->> TODO: keep in mind what happens if setEmotion() is called during a transition! <<--
+
+			// TODO:
+			// transition from one emotion to another
+			// implement as Promise chain? steps will overlap.
+
+			// transitions.unfocusContinents(currentEmotion)
+			// 1a. fade out and shrink circles of current continent;
+			// 1b. pull circles together toward center along horizontal axis as they fade/shrink
+
+			// transitions.panToContinent(currentEmotion)
+			// 1c. while 1a-b happens, pan toward continent location from current continent's location, according to all continents view layout.
+
+			// transitions.focusZoomedInContinent(currentEmotion)
+			// 2a. fade in and grow all circles for zoomed continent view from center of circle
+			//		random colors or picked from mocks?
+
+			// transitions.spreadFocusedContinent(currentEmotion)
+			// 2b. spread circles along horizontal axis as they fade in + grow
+			// 2c. (later) allow circles to drift slightly along horizontal axis only. this motion can be reflected in the states view as well.
+
+		} else {
+
+			// TODO:
+			// transition from all continents view into single continent view
+			// implement as Promise chain? steps will overlap.
+
+			// transitions.unfocusContinents(all other continents)
+			// 1. fade out and shrink all but focused continent
+
+			// transitions.panToContinent(currentEmotion)
+			// transitions.focusZoomedOutContinent(currentEmotion)
+			// 2a. zoom in on focused continent and pan to center
+			// 2b. while zooming, remove/add enough circles to match number of states
+			// 2c. while 2a-b happens, tween colors of circles to match mocks? or leave them randomized?
+
+			// transitions.spreadFocusedContinent()
+			// 2d. while 2a-c happens, execute (currentEmotion):2b-c above.
+		}
+
+	},
+
+	/**
+	 * Functions that perform transitions between continent views
+	 */
+	transitions: {
+
+		// 2a. zoom in on focused continent and pan to center
+		// 2b. while zooming, remove/add enough circles to match number of states
+		// 2c. while 2a-b happens, tween colors of circles to match mocks? or leave them randomized?
+		focusZoomedOutContinent: function (emotion) {
+
+		},
+
+		// 2a. fade in and grow all circles for zoomed continent view from center of circle
+		//		random colors or picked from mocks?
+		focusZoomedInContinent: function (emotion) {
+
+		},
+
+		// 1c. while 1a-b happens, pan toward continent location from current continent's location, according to all continents view layout.
+		panToContinent: function (emotion) {
+
+		},
+
+		// 2b. spread circles along horizontal axis as they fade in + grow
+		// 2c. (later) allow circles to drift slightly along horizontal axis only. this motion can be reflected in the states view as well.
+		spreadFocusedContinent: function (emotion) {
+
+		},
+
+		// 1. fade out and shrink all but focused continent / 
+		// 1a. fade out and shrink circles of continent;
+		// 1b. pull circles together toward center along horizontal axis as they fade/shrink
+		//		note: for zoomed-out continents, circles will already be centered, but that's ok.
+		unfocusContinents: function (emotions) {
+
+		},
 
 	},
 
