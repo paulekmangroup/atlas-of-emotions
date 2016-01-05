2bcfb60eba41fda522b72afa9da52731e712821f
ericsoco <eric@transmote.com>
2016-01-04 22:19:32 -0800

    Progress on exporter script

--- static/googleSheetsExporter.js
+++ static/googleSheetsExporter.js
@@ -45,6 +45,7 @@ function exportAll (e) {
 			} else {
 				acc.emotions[sheetName] = parseEmotionSheet(sheet);
 			}
+			return acc;
 		}, {
 			emotions: {}
 		});
@@ -59,14 +60,14 @@ function exportAll (e) {
  */
 function parseMetadataSheet (sheet) {
 
-	// top-left (0-indexed) of valid data in sheet
-	var DATA_START_COL = 1,
-		DATA_START_ROW = 2;
+	// top-left (1-indexed) of valid data in sheet
+	var DATA_START_COL = 2,
+		DATA_START_ROW = 3;
 
 	// walk down the section labels column,
 	// aggregate all rows in each section, and parse.
-	var labelsColumn = sheet.getSheetValues(DATA_START_COL, DATA_START_ROW, -1, 1);
-	Logger.log("metadata labelsColumn:", labelsColumn);
+	var labelsColumn = sheet.getSheetValues(DATA_START_ROW, DATA_START_COL, -1, 1);
+	Logger.log("metadata labelsColumn:" + labelsColumn);
 	var labelValue,
 		currentLabel,
 		metadata = {},
@@ -75,18 +76,26 @@ function parseMetadataSheet (sheet) {
 	for (var j=0; j<labelsColumn.length; j++) {
 
 		labelValue = labelsColumn[j][0];
-		Logger.log("metadata labelValue:", labelValue);
 		if (labelValue) { labelValue = labelValue.toLowerCase(); }
+		Logger.log("metadata labelValue:" + labelValue);
 
 		if (labelValue && labelValue !== currentLabel) {
-			// parse everything aggregated up to this next label
-			metadata[labelValue] = sectionParsers[labelValue](sectionData);
+			if (currentLabel) {
+				// parse everything aggregated up to this next label
+				Logger.log("sectionData:" + sectionData);
+				metadata[currentLabel] = metadataSectionParsers[currentLabel](sectionData);
+			}
+			Logger.log("set currentLabel to:" + labelValue);
 			currentLabel = labelValue;
-		} else {
-			// aggregate this row's data into sectionData
-			sectionData.push(sheet.getSheetValues(DATA_START_COL + 1, j, -1)[0]);
 		}
+
+		// aggregate this row's data into sectionData
+		sectionData.push(sheet.getSheetValues(DATA_START_ROW + j, DATA_START_COL + 1, 1, -1)[0]);
+
 	};
+
+	// parse everything left over
+	metadata[currentLabel] = metadataSectionParsers[currentLabel](sectionData);
 
 	return metadata;
 
@@ -95,53 +104,60 @@ function parseMetadataSheet (sheet) {
 /**
  * Atlas Of Emotions-specific parsing.
  */
-var metadataSectionParsers = {
+var metadataSectionParsers = (function () {
 
-	default: function (data) {
+	var standard = function (data) {
 		return data.map(function (row) {
 			return {
 				name: row[0],
 				desc: row[1]
 			};
 		});
-	},
+	};
 
-	continents: this.default,
+	return {
 
-	states: this.default,
+		intro: standard,
 
-	actions: this.default,
+		continents: standard,
 
-	triggers: function (data) {
-		var obj = {
-			header: data[0][0],
-			body: data[0][1]
-		};
-		obj.steps = data.map(function (row) {
-			return {
-				header: row[2],
-				body: row[3]
+		states: standard,
+
+		actions: standard,
+
+		triggers: function (data) {
+			Logger.log("triggers data:" + data);
+			var obj = {
+				header: data[0][0],
+				body: data[0][1]
 			};
-		});
-	},
+			obj.steps = data.map(function (row) {
+				return {
+					header: row[2],
+					body: row[3]
+				};
+			});
+		},
 
-	moods: this.default
+		moods: standard
 
-};
+	};
+
+})();
 
 /**
  * Atlas Of Emotions-specific parsing.
  */
 function parseEmotionSheet (sheet) {
 
-	// top-left (0-indexed) of valid data in sheet
-	var DATA_START_COL = 1,
-		DATA_START_ROW = 2;
+	// top-left (1-indexed) of valid data in sheet
+	var DATA_START_COL = 2,
+		DATA_START_ROW = 3;
 
 	// walk down the emotion section labels column,
 	// aggregate all rows in each section, and parse.
-	var labelsColumn = sheet.getSheetValues(DATA_START_COL, DATA_START_ROW, -1, 1);
-	Logger.log("emotion labelsColumn:", labelsColumn);
+	var labelsColumn = sheet.getSheetValues(DATA_START_ROW, DATA_START_COL, -1, 1);
+	Logger.log("emotion labelsColumn:" + labelsColumn);
 	var labelValue,
 		currentLabel,
 		emotionData = {},
@@ -150,18 +166,24 @@ function parseEmotionSheet (sheet) {
 	for (var j=0; j<labelsColumn.length; j++) {
 
 		labelValue = labelsColumn[j][0];
-		Logger.log("emotion labelValue:", labelValue);
 		if (labelValue) { labelValue = labelValue.toLowerCase(); }
+		Logger.log("emotion labelValue:" + labelValue);
 
 		if (labelValue && labelValue !== currentLabel) {
 			// parse everything aggregated up to this next label
-			emotionData[labelValue] = emotionSectionParsers[labelValue](sectionData);
+			if (currentLabel) {
+				emotionData[currentLabel] = emotionSectionParsers[currentLabel](sectionData);
+			}
 			currentLabel = labelValue;
-		} else {
-			// aggregate this row's data into sectionData
-			sectionData.push(sheet.getSheetValues(DATA_START_COL + 1, j, -1)[0]);
 		}
+
+		// aggregate this row's data into sectionData
+		sectionData.push(sheet.getSheetValues(DATA_START_ROW + j, DATA_START_COL + 1, 1, -1)[0]);
+
 	};
+
+	// parse everything left over
+	emotionData[currentLabel] = emotionSectionParsers[currentLabel](sectionData);
 
 	return emotionData;
 
@@ -170,66 +192,58 @@ function parseEmotionSheet (sheet) {
 /**
  * Atlas Of Emotions-specific parsing.
  */
-var emotionSectionParsers = {
+var emotionSectionParsers = (function () {
 
-	continent: function (data) {
+	var standard = function (data) {
 		return data.map(function (row) {
 			return {
 				name: row[0],
 				desc: row[1]
 			};
 		});
-	},
+	};
 
-	states: function (data) {
-		return data.map(function (row) {
-			var con = (row[2] || []).map(function (val) { return val.toLowerCase(); }),
-				des = (row[3] || []).map(function (val) { return val.toLowerCase(); }),
-				all = con.concat();
+	return {
 
-			des.forEach(function (val) {
-				if (!~all.indexOf(val)) {
-					all.push(val);
-				}
+		continent: standard,
+
+		states: function (data) {
+			return data.map(function (row) {
+				var con = (row[2] || []).map(function (val) { return val.toLowerCase(); }),
+					des = (row[3] || []).map(function (val) { return val.toLowerCase(); }),
+					all = con.concat();
+
+				des.forEach(function (val) {
+					if (!~all.indexOf(val)) {
+						all.push(val);
+					}
+				});
+
+				return {
+					name: row[0],
+					desc: row[1],
+					actions: {
+						all: all,
+						con: con,
+						des: des
+					}
+				};
 			});
+		},
 
-			return {
-				name: row[0],
-				desc: row[1],
-				actions: {
-					all: all,
-					con: con,
-					des: des
-				}
-			};
-		});
-	},
+		actions: standard,
 
-	actions: function (data) {
-		return data.map(function (row) {
-			return {
-				name: row[0],
-				desc: row[1]
-			};
-		});
-	},
+		triggers: function (data) {
+			return data.map(function (row) {
+				return row[0];
+			});
+		},
 
-	triggers: function (data) {
-		return data.map(function (row) {
-			return row[0];
-		});
-	},
+		moods: standard
 
-	moods: function (data) {
-		return data.map(function (row) {
-			return {
-				name: row[0],
-				desc: row[1]
-			};
-		});
-	}
+	};
 
-};
+})();
 
 /**
  * Output parsed sheets for consumption by end user.
