843632077f70a1bd047acd67f1435ab72cdaf5ec
Jasper Speicher <jasper.speicher@gmail.com>
2017-03-07 18:07:47 -0800

    Update timeline prototype to work better with atlas.

--- src/timeline/Episode.js
+++ src/timeline/Episode.js
@@ -1,189 +1,191 @@
-/**
- *
- * no awareness
- *
- */
+import Continent from '../Continent.js';
+import timeline from './timeline.js'
+import { TweenMax, TimelineMax } from "gsap";
+
+export default class Episode {
+
+	constructor( svg, container ) {
+
+		var configsByEmotion = Continent.configsByEmotion;
+
+		if ( svg && !svg._initializedEpisode ) {
 
 
-function Episode( svgDoc, object ) {
-
-    if ( svgDoc && !svgDoc._initializedEpisode ) {
-
-
-        //fonts need to be added for them to work in svg
-        addFonts( svgDoc );
+			//fonts need to be added for them to work in svg
+			timeline.addFonts( svg );
 
 
-        var parent = extractDocument( svgDoc, object );
+			var parent = timeline.extractDocument( svg, container );
 
-        var content = [
-            { event: 'a stranger shouts at you', state: 'fear', response: 'you run away' },
-            { event: 'a stranger shouts at you', state: 'anger', response: 'you shout back' },
-            { event: 'a stranger shouts at you', state: 'enjoyment', response: 'you laugh!' }
-        ];
-
-        var svg = select( 'svg', parent );
-
-        TweenMax.allTo( getChildren( select( '#state', parent ) ), 0, { visibility: 'hidden' } );
-
-        //state
-        var state = select( '#state', parent ),
-            stateLabel = select( '#state-label', parent );
-        var bBox = state.getBBox();
-        var stateLabelChildren = getChildren( stateLabel );
-        for ( var i = 0; i < stateLabelChildren.length; i++ ) {
-            stateLabelChildren[ i ].setAttribute( 'x', bBox.x + bBox.width / 2 );
-            stateLabelChildren[ i ].setAttribute( 'text-anchor', 'middle' );
-            if ( i == 1 ) {
-                stateLabelChildren[ i ].style.textTransform = 'uppercase';
-            }
-        }
-
-        //circles
-        var c1 = select( '#c1', parent ),
-            c2 = select( '#c2', parent ),
-            c3 = select( '#c3', parent );
-
-        //lines
-        var eventLines = select( '#event-lines', parent ),
-            responseLines = select( '#response-lines', parent );
-
-        //text
-        var event = select( '#event', parent ),
-            responses = select( '#responses', parent );
-        var responseChildren = selectAll( 'tspan', responses );
-
-        //try again button
-        var tryAgainButton = select( '#try-again', parent );
-        tryAgainButton.style.cursor = 'pointer';
-        tryAgainButton.style.pointerEvents = 'all';
-        var tryAgainLabel = select( '#try-again-label tspan', tryAgainButton );
-        var tryAgainRectangle = select( '#try-again-rectangle', tryAgainButton );
-
-        var tl = new TimelineMax( {} );
-
-        var contentIndex = 0;
-
-        var replaceContent = function ( contentIndex ) {
-
-            var colors;
-            stateLabelChildren[ 1 ].textContent = content[ contentIndex ].state;
-            responseChildren[ 0 ].textContent = content[ contentIndex ].response;
-            colors = configsByEmotion[ content[ contentIndex ].state ].colorPalette;
-
-            var color1 = colors[ 0 ];
-            var color2 = colors[ Math.round( (colors.length - 1) / 3 ) ];
-            var color3 = colors[ colors.length - 1 ];
-            c1.setAttribute( 'fill', 'rgb(' + color1[ 0 ] + ',' + color1[ 1 ] + ',' + color1[ 2 ] + ')' );
-            c2.setAttribute( 'fill', 'rgb(' + color2[ 0 ] + ',' + color2[ 1 ] + ',' + color2[ 2 ] + ')' );
-            c3.setAttribute( 'fill', 'rgb(' + color3[ 0 ] + ',' + color3[ 1 ] + ',' + color3[ 2 ] + ')' );
-
-        };
-
-        var reset = function () {
-
-            tl.timeScale( 3 );
-
-            tl.pause();
-            tl.seek( 'end' );
-
-            tl.tweenTo( 'event-lines', {
-                onComplete: function () {
-
-                    contentIndex++;
-                    if ( contentIndex >= content.length ) {
-                        contentIndex = 0;
-                    }
-
-                    //replace content
-                    replaceContent( contentIndex );
-
-                    //run animation
-                    tl.timeScale( 1 );
-                    tl.pause();
-                    tl.seek( 'event-pulse' );
-
-                    TweenMax.delayedCall( 0.5, function () {
-                        tl.play();
-                    } );
-
-                }, ease: Power2.easeOut
-            } );
-
-        };
-
-        //make sure event pulsates from center
-        event.style.transformOrigin = "50% 50%";
-
-        //start the timeline
+			var content = [
+				{ event: 'a stranger shouts at you', state: 'fear', response: 'you run away' },
+				{ event: 'a stranger shouts at you', state: 'anger', response: 'you shout back' },
+				{ event: 'a stranger shouts at you', state: 'enjoyment', response: 'you laugh!' }
+			];
 
 
-        tl
-        //show event
-            .add( 'event' )
-            .from( event, 0.5, { autoAlpha: 0, ease: Power1.easeOut } )
+			TweenMax.allTo( timeline.getChildren( timeline.select( '#state', parent ) ), 0, { visibility: 'hidden' } );
 
-            .add( 'event-pulse' )
-            .to( event, 0.1, { scale: 1.1, ease: Power1.easeOut } )
-            .to( event, 0.2, { scale: 1, ease: Power1.easeOut } )
-            .add( 'event-lines' )
-            .from( eventLines, 0.5, { autoAlpha: 0, ease: Power1.easeOut }, 'event-lines' )
+			//state
+			var state = timeline.select( '#state', parent ),
+				stateLabel = timeline.select( '#state-label', parent );
+			var bBox = state.getBBox();
+			var stateLabelChildren = timeline.getChildren( stateLabel );
+			for ( var i = 0; i < stateLabelChildren.length; i++ ) {
+				stateLabelChildren[ i ].setAttribute( 'x', bBox.x + bBox.width / 2 );
+				stateLabelChildren[ i ].setAttribute( 'text-anchor', 'middle' );
+				if ( i == 1 ) {
+					stateLabelChildren[ i ].style.textTransform = 'uppercase';
+				}
+			}
 
-            // show emo state
-            .add( 'state' )
-            .from( c1, 2, { attr: { r: 0 }, autoAlpha: 0, ease: Bounce.easeOut } )
-            .from( c2, 2, { attr: { r: 20 }, autoAlpha: 0, ease: Bounce.easeOut }, 'state' )
-            .from( c3, 2, { attr: { r: 50 }, autoAlpha: 0, ease: Bounce.easeOut }, 'state' )
-            .from( stateLabel, 2, { autoAlpha: 0, ease: Power1.easeOut }, 'state' )
-            .add( 'pulsate' )
+			//circles
+			var c1 = timeline.select( '#c1', parent ),
+				c2 = timeline.select( '#c2', parent ),
+				c3 = timeline.select( '#c3', parent );
 
-            //show response
-            .add( 'response-line', '-=1' )
-            .from( responseLines, 0.5, { autoAlpha: 0, ease: Power1.easeOut }, 'response-line' )
+			//lines
+			var eventLines = timeline.select( '#event-lines', parent ),
+				responseLines = timeline.select( '#response-lines', parent );
 
-            .add( 'responses', '-=0.5' )
-            .from( responses, 1, { autoAlpha: 0, ease: Power1.easeOut } )
+			//text
+			var event = timeline.select( '#event', parent ),
+				responses = timeline.select( '#responses', parent );
+			var responseChildren = timeline.selectAll( 'tspan', responses );
 
-            .add( 'try-again' )
-            .from( tryAgainButton, 0.5, { autoAlpha: 0, ease: Power1.easeOut } )
+			//try again button
+			var tryAgainButton = timeline.select( '#try-again', parent );
+			tryAgainButton.style.cursor = 'pointer';
+			tryAgainButton.style.pointerEvents = 'all';
+			var tryAgainLabel = timeline.select( '#try-again-label tspan', tryAgainButton );
+			var tryAgainRectangle = timeline.select( '#try-again-rectangle', tryAgainButton );
 
-            .add( 'end' )
+			var tl = new TimelineMax( {} );
 
-            //pulsate state
-            .to( c1, 1, {
-                attr: { r: '-=5' },
-                repeat: -1,
-                yoyo: true,
-                repeatDelay: 0,
-                ease: Power1.easeInOut
-            }, 'pulsate' )
-            .to( c2, 0.8, {
-                attr: { r: '-=3' },
-                repeat: -1,
-                yoyo: true,
-                repeatDelay: 0,
-                ease: Power1.easeInOut
-            }, 'pulsate' )
-            .to( c3, 1, {
-                attr: { r: '-=2' },
-                repeat: -1,
-                yoyo: true,
-                repeatDelay: 0,
-                ease: Power1.easeInOut
-            }, 'pulsate' );
+			var contentIndex = 0;
 
-        tryAgainButton.onclick = function () {
-            reset();
-        };
+			var replaceContent = function ( contentIndex ) {
 
-        TweenMax.set( state, { visibility: 'visible' } );
+				var colors;
+				stateLabelChildren[ 1 ].textContent = content[ contentIndex ].state;
+				responseChildren[ 0 ].textContent = content[ contentIndex ].response;
+				colors = configsByEmotion[ content[ contentIndex ].state ].colorPalette;
 
-        replaceContent( contentIndex );
+				var color1 = colors[ 0 ];
+				var color2 = colors[ Math.round( (colors.length - 1) / 3 ) ];
+				var color3 = colors[ colors.length - 1 ];
+				c1.setAttribute( 'fill', 'rgb(' + color1[ 0 ] + ',' + color1[ 1 ] + ',' + color1[ 2 ] + ')' );
+				c2.setAttribute( 'fill', 'rgb(' + color2[ 0 ] + ',' + color2[ 1 ] + ',' + color2[ 2 ] + ')' );
+				c3.setAttribute( 'fill', 'rgb(' + color3[ 0 ] + ',' + color3[ 1 ] + ',' + color3[ 2 ] + ')' );
 
-        TweenMax.set( parent, { visibility: 'visible' } );
+			};
 
-        svgDoc._initializedEpisode = true;
+			var reset = function () {
 
-    }
+				tl.timeScale( 3 );
+
+				tl.pause();
+				tl.seek( 'end' );
+
+				tl.tweenTo( 'event-lines', {
+					onComplete: function () {
+
+						contentIndex++;
+						if ( contentIndex >= content.length ) {
+							contentIndex = 0;
+						}
+
+						//replace content
+						replaceContent( contentIndex );
+
+						//run animation
+						tl.timeScale( 1 );
+						tl.pause();
+						tl.seek( 'event-pulse' );
+
+						TweenMax.delayedCall( 0.5, function () {
+							tl.play();
+						} );
+
+					}, ease: Power2.easeOut
+				} );
+
+			};
+
+			//make sure event pulsates from center
+			event.style.transformOrigin = "50% 50%";
+
+			//start the timeline
+
+
+			tl
+			//show event
+				.add( 'event' )
+				.from( event, 0.5, { autoAlpha: 0, ease: Power1.easeOut } )
+
+				.add( 'event-pulse' )
+				.to( event, 0.1, { scale: 1.1, ease: Power1.easeOut } )
+				.to( event, 0.2, { scale: 1, ease: Power1.easeOut } )
+				.add( 'event-lines' )
+				.from( eventLines, 0.5, { autoAlpha: 0, ease: Power1.easeOut }, 'event-lines' )
+
+				// show emo state
+				.add( 'state' )
+				.from( c1, 2, { attr: { r: 0 }, autoAlpha: 0, ease: Bounce.easeOut } )
+				.from( c2, 2, { attr: { r: 20 }, autoAlpha: 0, ease: Bounce.easeOut }, 'state' )
+				.from( c3, 2, { attr: { r: 50 }, autoAlpha: 0, ease: Bounce.easeOut }, 'state' )
+				.from( stateLabel, 2, { autoAlpha: 0, ease: Power1.easeOut }, 'state' )
+				.add( 'pulsate' )
+
+				//show response
+				.add( 'response-line', '-=1' )
+				.from( responseLines, 0.5, { autoAlpha: 0, ease: Power1.easeOut }, 'response-line' )
+
+				.add( 'responses', '-=0.5' )
+				.from( responses, 1, { autoAlpha: 0, ease: Power1.easeOut } )
+
+				.add( 'try-again' )
+				.from( tryAgainButton, 0.5, { autoAlpha: 0, ease: Power1.easeOut } )
+
+				.add( 'end' )
+
+				//pulsate state
+				.to( c1, 1, {
+					attr: { r: '-=5' },
+					repeat: -1,
+					yoyo: true,
+					repeatDelay: 0,
+					ease: Power1.easeInOut
+				}, 'pulsate' )
+				.to( c2, 0.8, {
+					attr: { r: '-=3' },
+					repeat: -1,
+					yoyo: true,
+					repeatDelay: 0,
+					ease: Power1.easeInOut
+				}, 'pulsate' )
+				.to( c3, 1, {
+					attr: { r: '-=2' },
+					repeat: -1,
+					yoyo: true,
+					repeatDelay: 0,
+					ease: Power1.easeInOut
+				}, 'pulsate' );
+
+
+			tryAgainButton.onclick = function () {
+				reset();
+			};
+
+			TweenMax.set( state, { visibility: 'visible' } );
+
+			replaceContent( contentIndex );
+
+			TweenMax.set( parent, { visibility: 'visible' } );
+
+			svg._initializedEpisode = true;
+
+		}
+	}
 
 }
--- src/timeline/timeline.js
+++ src/timeline/timeline.js
@@ -1,210 +1,158 @@
-//work around safari missing children property in svg nodes
-function getChildren( element ) {
-    var svgChildren = element.children || element.childNodes;
-    var children = [];
-    for ( var i = 0; i < svgChildren.length; i++ ) {
-        if ( svgChildren[ i ].nodeType == Node.TEXT_NODE || svgChildren[ i ].nodeType == Node.COMMENT_NODE ) continue;
-        children.push( svgChildren[ i ] );
-    }
-    return children;
-}
+//TODO this file contains a bunch of low level stuff that probably belongs elsewhere or is in d3 etc
+import Episode from './Episode.js';
 
-function addFonts( svgDoc ) {
-    var fontCss = "@import url('https://fonts.googleapis.com/css?family=Raleway');",
-        style = document.createElement( 'style' );
+const timeline = {
+	isInited: false,
+	isActive: false,
+	screenIsSmall: false,
+	displayingIntro: false,
+	currentEmotion: null,
 
-    style.type = 'text/css';
+	//work around safari missing children property in svg nodes
+	getChildren: function ( element ) {
+		var svgChildren = element.children || element.childNodes;
+		var children = [];
+		for ( var i = 0; i < svgChildren.length; i++ ) {
+			if ( svgChildren[ i ].nodeType == Node.TEXT_NODE || svgChildren[ i ].nodeType == Node.COMMENT_NODE ) continue;
+			children.push( svgChildren[ i ] );
+		}
+		return children;
+	},
 
-    if ( style.styleSheet ) {
-        style.styleSheet.cssText = fontCss;
-    } else {
-        style.appendChild( document.createTextNode( fontCss ) );
-    }
+	addFonts: function ( svg ) {
+		var fontCss = "@import url('https://fonts.googleapis.com/css?family=Raleway');",
+			style = document.createElement( 'style' );
 
-    svgDoc.getElementsByTagName( 'defs' )[ 0 ].appendChild( style );
-}
+		style.type = 'text/css';
 
-function select( selector, parent ) {
-    return parent.querySelector( selector );
-}
+		if ( style.styleSheet ) {
+			style.styleSheet.cssText = fontCss;
+		} else {
+			style.appendChild( document.createTextNode( fontCss ) );
+		}
 
-function selectAll( selector, parent ) {
-    return parent.querySelectorAll( selector );
-}
+		svg.getElementsByTagName( 'defs' )[ 0 ].appendChild( style );
+	},
 
+	select: function ( selector, parent ) {
+		return parent.querySelector( selector );
+	},
 
-function extractDocument( svgDoc, object ) {
-
-    var NS = "http://www.w3.org/2000/svg";
-
-    // pull svg element out of the document and make it self center so that
-    // illumination can be animated across it
-    var svgElement = svgDoc.getElementsByTagName( 'svg' )[ 0 ];
-
-    var originalWidth = svgElement.getAttribute( "width" ),
-        originalHeight = svgElement.getAttribute( 'height' ),
-        originalViewBox = svgElement.getAttribute( 'viewBox' );
-    svgElement.setAttribute( 'overflow', 'visible' );
-    svgElement.setAttribute( 'width', '100%' );
-    svgElement.setAttribute( 'height', '100%' );
-    svgElement.removeAttribute( 'viewBox' );
-    svgElement.setAttribute( 'preserveAspectRatio', 'none' );
-    var svgChildrenGroup = svgDoc.getElementsByTagName( 'g' )[ 0 ];
-
-    var wrapperSVG = document.createElementNS( NS, 'svg' );
-    wrapperSVG.setAttribute( 'x', '50%' );
-    wrapperSVG.setAttribute( 'y', '50%' );
-    wrapperSVG.setAttribute( 'width', originalWidth );
-    wrapperSVG.setAttribute( 'height', originalHeight );
-    wrapperSVG.removeAttribute( 'viewBox' );
-    wrapperSVG.setAttribute( 'overflow', 'visible' );
-    wrapperSVG.appendChild( svgChildrenGroup );
-
-    var wrapperGroup = document.createElementNS( NS, 'g' );
-    wrapperGroup.setAttribute( 'transform', 'translate(' + (-0.5 * parseInt( originalWidth )) + ', ' + (-0.5 * parseInt( originalHeight )) + ')' );
-    wrapperGroup.appendChild( wrapperSVG );
-
-    svgElement.appendChild( wrapperGroup );
-
-    var newParentDiv = document.createElement( 'div' );
-    newParentDiv.classList.add( 'episode-parent' );
-    newParentDiv.appendChild( svgElement );
-    object.parentNode.appendChild( newParentDiv );
-    object.remove();
-
-    return newParentDiv;
-
-}
-
-/**
- *
- * Initialize
- *
- **/
+	selectAll: function ( selector, parent ) {
+		return parent.querySelectorAll( selector );
+	},
 
 
+	extractDocument: function ( svgElement, container ) {
 
-var configsByEmotion = {
-    'anger': {
-        colorPalette: [
-            [ 204, 28, 23 ],
-            [ 208, 46, 53 ],
-            [ 212, 65, 63 ],
-            [ 216, 83, 73 ],
-            [ 220, 102, 83 ],
-            [ 224, 120, 92 ],
-            [ 228, 135, 102 ]
-        ]
-    },
-    'disgust': {
-        colorPalette: [
-            [ 0, 104, 55 ],
-            [ 0, 110, 57 ],
-            [ 0, 116, 59 ],
-            [ 0, 122, 61 ],
-            [ 0, 128, 63 ],
-            [ 0, 130, 65 ],
-            [ 0, 136, 67 ],
-            [ 0, 142, 69 ]
-        ]
-    },
-    'enjoyment': {
-        colorPalette: [
-            [ 248, 136, 29 ],
-            [ 243, 143, 30 ],
-            [ 243, 136, 33 ],
-            [ 244, 149, 36 ],
-            [ 244, 153, 40 ],
-            [ 245, 156, 43 ],
-            [ 245, 159, 46 ],
-            [ 246, 162, 49 ],
-            [ 247, 169, 56 ],
-            [ 246, 166, 53 ],
-            [ 247, 172, 59 ]
-        ]
-    },
-    'fear': {
-        colorPalette: [
-            [ 143, 39, 139 ],
-            [ 156, 41, 153 ],
-            [ 196, 49, 194 ],
-            [ 209, 51, 207 ],
-            [ 223, 53, 221 ],
-            [ 235, 56, 234 ],
-            [ 248, 58, 248 ]
-        ]
-    },
-    'sadness': {
-        colorPalette: [
-            [ 65, 74, 161 ],
-            [ 54, 104, 178 ],
-            [ 49, 124, 189 ],
-            [ 44, 139, 200 ],
-            [ 51, 158, 211 ],
-            [ 85, 172, 217 ],
-            [ 146, 198, 229 ],
-            [ 174, 209, 234 ],
-            [ 195, 218, 238 ]
-        ]
-    }
+		var NS = "http://www.w3.org/2000/svg";
+
+		// pull svg element out of the document and make it self center so that
+		// illumination can be animated across it
+
+		var originalWidth = svgElement.getAttribute( "width" ),
+			originalHeight = svgElement.getAttribute( 'height' ),
+			originalViewBox = svgElement.getAttribute( 'viewBox' );
+		svgElement.setAttribute( 'overflow', 'visible' );
+		svgElement.setAttribute( 'width', '100%' );
+		svgElement.setAttribute( 'height', '100%' );
+		svgElement.removeAttribute( 'viewBox' );
+		svgElement.setAttribute( 'preserveAspectRatio', 'none' );
+		var svgChildrenGroup = svgElement.getElementsByTagName( 'g' )[ 0 ];
+
+		var wrapperSVG = document.createElementNS( NS, 'svg' );
+		wrapperSVG.setAttribute( 'x', '50%' );
+		wrapperSVG.setAttribute( 'y', '50%' );
+		wrapperSVG.setAttribute( 'width', originalWidth );
+		wrapperSVG.setAttribute( 'height', originalHeight );
+		wrapperSVG.removeAttribute( 'viewBox' );
+		wrapperSVG.setAttribute( 'overflow', 'visible' );
+		wrapperSVG.appendChild( svgChildrenGroup );
+
+		var wrapperGroup = document.createElementNS( NS, 'g' );
+		wrapperGroup.setAttribute( 'transform', 'translate(' + (-0.5 * parseInt( originalWidth )) + ', ' + (-0.5 * parseInt( originalHeight )) + ')' );
+		wrapperGroup.appendChild( wrapperSVG );
+
+		svgElement.appendChild( wrapperGroup );
+
+		var newParentDiv = document.createElement( 'div' );
+		newParentDiv.classList.add( 'episode-parent' );
+		newParentDiv.appendChild( svgElement );
+		container.appendChild( newParentDiv );
+
+		return newParentDiv;
+
+	},
+
+	loadEpisode: function () {
+		var ajax = new XMLHttpRequest();
+		ajax.open( 'GET', '/img/episode.svg', true )
+		ajax.send()
+		ajax.onload = function ( e ) {
+			// no awareness version
+			var container = document.getElementById( 'timeline-graphics' );
+			var episode = new Episode( e.currentTarget.responseXML.documentElement, container );
+		}
+	},
+
+	loadEpisodeAddAwareness: function () {
+		// awareness version
+		var episodeObjectAddAwareness = document.getElementById( 'episode-object--add-awareness' );
+		var episodeAddAwareness = new EpisodeAddAwareness( episodeObjectAddAwareness.getSVGDocument(), episodeObjectAddAwareness );
+	},
+
+	init: function ( containerNode, screenIsSmall ) {
+
+		this.sectionContainer = containerNode;
+
+		this.screenIsSmall = screenIsSmall;
+
+		this.loadEpisode();
+
+		this.isInited = true;
+
+	},
+
+	setActive: function ( val ) {
+
+		let section = this;
+		this.isActive = val;
+
+	},
+
+	setInteractive: function ( val ) {
+
+	},
+
+	setEmotion: function ( emotion, previousEmotion ) {
+
+		var _self = this;
+
+		return new Promise( ( resolve, reject ) => {
+			_self.currentEmotion = emotion;
+			resolve();
+		} );
+
+	},
+
+	open: function ( options ) {
+
+		this.setActive( true );
+		this.setInteractive( true );
+
+	},
+
+	close: function ( nextSection ) {
+
+		return new Promise( ( resolve, reject ) => {
+
+			this.setActive( false );
+			this.setInteractive( false );
+
+			resolve();
+		} );
+	}
+
 };
 
-var episode = null;
-
-var episodeAddAwareness = null;
-
-var resourcesLoaded = 0;
-var resourcesCount = 2;
-var episodeObject = document.getElementById( 'episode-object' );
-var episodeObjectAddAwareness = document.getElementById( 'episode-object--add-awareness' );
-
-var loadInteractions = function () {
-    $( '#fullpage' ).fullpage( {
-        anchors: [ 'episode-section', 'episode-section--add-awareness' ],
-        afterLoad: function ( anchorLink, index ) {
-            //console.log( 'loaded ' + anchorLink );
-            if ( anchorLink == 'episode-section' && episode == null ) {
-                // no awareness version
-                setTimeout( function () {
-                    episode = new Episode( episodeObject.getSVGDocument(), episodeObject );
-                }, 1000 );
-            } else if ( anchorLink == 'episode-section--add-awareness' && episodeAddAwareness == null ) {
-                // awareness version
-                setTimeout( function () {
-                    episodeAddAwareness = new EpisodeAddAwareness( episodeObjectAddAwareness.getSVGDocument(), episodeObjectAddAwareness );
-                }, 1000 );
-            }
-
-        }
-    } );
-};
-
-var registerResourceLoad = function () {
-    resourcesLoaded++;
-    //console.log('loaded '+resourcesLoaded);
-    if ( resourcesLoaded == resourcesCount ) {
-        loadInteractions();
-    }
-}
-
-if( episodeObject.getSVGDocument() == null){
-
-    episodeObject.addEventListener( 'load', function () {
-        //episode = new Episode( episodeObject.getSVGDocument(), episodeObject );
-        registerResourceLoad();
-    } );
-
-}else{
-    registerResourceLoad();
-}
-
-if( episodeObjectAddAwareness.getSVGDocument() == null) {
-
-    episodeObjectAddAwareness.addEventListener( 'load', function () {
-        //episodeAddAwareness = new EpisodeAddAwareness( episodeObjectAddAwareness.getSVGDocument(), episodeObjectAddAwareness );
-        registerResourceLoad();
-    } );
-
-}else{
-    registerResourceLoad();
-}
+export default timeline;
