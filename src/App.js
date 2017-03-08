93cdeb3d39c833874068593cb72a96b622c886e5
Jasper Speicher <jasper.speicher@gmail.com>
2017-03-07 18:06:15 -0800

    Don't init section nav because it is not used any more

--- src/App.js
+++ src/App.js
@@ -83,7 +83,7 @@ export default function ( ...initArgs ) {
 				initContainers();
 				initSections();
 				initHeader();
-				initSectionNavigation();
+				//initSectionNavigation();
 				initNavArrows();
 				initMoreInfoDropdown();
 				initPegLogo();
